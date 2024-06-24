b6b4e716d8af9e91e49da084675fb3afab3b90d3
lobna3 <yousfi55lobna@gmail.com>
2024-06-24 13:42:29 +0200

    create users models && controllers && routes

--- BackEnd/controllers/users.controllers.js
+++ BackEnd/controllers/users.controllers.js
@@ -1,20 +1,32 @@
 const UserModel = require("../database/models/users.models.js");
+const ValidateRegister = require("../validation/Register");
+const bcrypt = require('bcryptjs')
 
 const Register = async (req, res) => {
+    const { errors, isValid } = ValidateRegister(req.body);
     try {
-        await UserModel.create(req.body)
-        res.status(200).json({ message: "success" })
-
+      if (!isValid) {
+        res.status(404).json(errors);
+      } else {
+        UserModel.findOne({ email: req.body.email }).then(async(exist) => {
+          if (exist) {
+            errors.email = "user exist";
+            res.status(404).json(errors);
+          } else {
+            const hash = bcrypt.hashSync(req.body.password, 10)//hashed password
+            req.body.password = hash;
+            req.body.role = "USER";
+            await UserModel.create(req.body);
+            res.status(200).json({ message: "success" });
+          }
+        });
+      }
     } catch (error) {
-           res.status(500).json(error)
+      res.status(404).json(error.message);
     }
-
-
-}
+  };
 
 
 module.exports = {
-
-
     Register
 }
