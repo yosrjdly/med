8f24bb5eaf8ab38502254ed9642a966bfe23a851
lobna3 <yousfi55lobna@gmail.com>
2024-06-25 12:40:21 +0200

    npm react-router-dom

--- FrontEnd/medical-app/src/App.js
+++ FrontEnd/medical-app/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import './App.css';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello My App</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
