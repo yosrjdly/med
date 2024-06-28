69a8fd7f9bfbb9eb89f6804fdabe40e8fbbfbde3
samir <samirabidi1123@gmail.com>
2024-06-28 17:03:51 +0100

    create state for file Uploadimage

---
+++ FrontEnd/medical-app/src/componentPatient/UploadImage.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const UploadImage = () => {
+    const [name, setName] = useState("")
+    const [image, setImage] = useState(null);
+    const [imageUrl, setImageUrl] = useState(null);
+    const [loading, setLoading] = useState(false);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UploadImage
