e11282610bcca367b0a6f7527fa3a6aff509246c
samir <samirabidi1123@gmail.com>
2024-06-28 17:10:56 +0100

    create a function file upload

--- FrontEnd/medical-app/src/componentPatient/UploadImage.jsx
+++ FrontEnd/medical-app/src/componentPatient/UploadImage.jsx
@@ -5,6 +5,22 @@ const UploadImage = () => {
     const [image, setImage] = useState(null);
     const [imageUrl, setImageUrl] = useState(null);
     const [loading, setLoading] = useState(false);
+    const uploadFile = async () => {
+        const data = new FormData();
+        data.append("file", image); 
+        data.append("upload_preset", "tested");
+        try {
+            const cloudName = "dxjndurf9";
+            const resourceType = "image"; 
+            const api = `https://api.cloudinary.com/v1_1/${cloudName}/${resourceType}/upload`;
+            const res = await axios.post(api, data);
+            const { url } = res.data;
+            console.log(url);
+            return url;
+        } catch (error) {
+            console.log(error);
+        }
+    }
   return (
     <div>
       
